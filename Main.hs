aa35dd0266861a87d90d35fcf11fc5d092237cea
jheave <jimenezjheave123@gmail.com>
2022-06-14 21:14:47 +0800

    add message function

--- Main.hs
+++ Main.hs
@@ -1,20 +1,20 @@
 {--
     Number guessing game
-    -- Ask the user to stake a bet
     -- Generate a random number between 1 and 100
     -- Ask the user to guess the number
     -- If the user guesses the number, congratulate them
     -- If the user guesses incorrectly, tell them if they guessed too high or too low
-    -- If the user guesses correctly return the amount that he user bet * 2.5 / guess count
     -- If the user runs out of guesses, tell them they lost
 --}
 
 module Main where
 import System.Random
-import Control.Monad ()
+import System.Exit (die)
 
-generateRandomNumber :: IO Int
-generateRandomNumber = getStdRandom (randomR (1, 100))
+message :: String
+message = "Welcome to the number guessing game! \n\
+          \You have 10 guesses to guess the number.\n\
+          \The computer will generate a random number between 1 and 100.\n\
+          \Try to guess it in as few attempts as possible.\n"
 
-main :: IO () 
-main = show generateRandomNumber
+
